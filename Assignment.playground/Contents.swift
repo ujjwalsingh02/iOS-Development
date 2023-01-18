73362d8b9bcffc901f992a0c49e7729e5f1d09c4
Ujjwal <101248846+ujjwalsingh02@users.noreply.github.com>
2023-01-18 22:37:35 +0530

    Add files via upload

---
+++ Assignment.playground/Contents.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+//var greeting = "Hello, playground"
+
+//Q1
+//var x = 0
+//var y = 0
+////var point = (0,0)
+//var point = (x, y)
+//
+//enum moveDirection{
+//    case up
+//    case down
+//    case left
+//    case right
+//}
+//
+//var movement = moveDirection.up
+//movement = moveDirection.left
+//
+//switch movement{
+//case.right :
+//    x = x + 1
+//case.left :
+//    x = x - 1
+//case.up :
+//    y = y + 1
+//case.down :
+//    y = y - 1
+//}
+////print(x)
+////print(y)
+////print(point)
+//print("The current x point is \(x) and y is \(y)")
+
+
+//Q2
+//func divideIfWhole(a : Int, b : Int) -> Int?{
+//    if(a%b == 0){
+//        return a/b
+//    }
+//    else {
+//        return nil
+//    }
+//}
+//func check(_ a: Int,_ b: Int) {
+//    if var answer = divideIfWhole(a: a, b: b) {
+//        print("Yep! it divides \(answer) times.")
+//    }
+//    else{
+//        print("Not divisible.")
+//    }
+//}
+//check(10, 2)
+//check(10, 3)
