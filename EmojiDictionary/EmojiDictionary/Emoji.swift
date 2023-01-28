a179c3b89d8feb299b61e93f5ed115a37a301bfd
Ujjwal <101248846+ujjwalsingh02@users.noreply.github.com>
2023-01-28 22:05:24 +0530

    Add files via upload

---
+++ EmojiDictionary/EmojiDictionary/Emoji.swift
@@ -0,0 +1,15 @@
+//
+//  Emoji.swift
+//  EmojiDictionary
+//
+//  Created by Ujjwalsingh Rajput on 11/01/23.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
