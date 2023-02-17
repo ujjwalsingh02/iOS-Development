3af8c97eaa01178b82ee81ae3ad64559914b0596
Ujjwal <101248846+ujjwalsingh02@users.noreply.github.com>
2023-02-17 22:10:45 +0530

    Add files via upload

---
+++ ScrollViews/ISpy/ISpy/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  ISpy
+//
+//  Created by Ujjwalsingh Rajput on 13/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        scrollView.delegate = self
+        updateZoomFor(view.bounds.size)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func updateZoomFor(_ size: CGSize) {
+        let widthScale = size.width / imageView.bounds.width
+        let heightScale = size.height / imageView.bounds.height
+        let scale = min(widthScale, heightScale)
+        
+        scrollView.minimumZoomScale = scale
+    }
+
+
+}
+
+extension ViewController: UIScrollViewDelegate {
+
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+}
+
